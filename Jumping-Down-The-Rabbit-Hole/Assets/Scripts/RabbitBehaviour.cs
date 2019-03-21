566d477a37d23da18b3bd46a5eee3816a3b1e4f1
falah <falah.shazib@gmail.com>
2019-03-21 01:07:06 -0400

    scene w/ hopping

--- Jumping-Down-The-Rabbit-Hole/Assets/Scripts/RabbitBehaviour.cs
+++ Jumping-Down-The-Rabbit-Hole/Assets/Scripts/RabbitBehaviour.cs
@@ -5,28 +5,34 @@ using UnityEngine;
 public class RabbitBehaviour : MonoBehaviour
 {
 	public GameObject[] checkpoints;
-	//public GameObject rabbitModel;
+
 	private int currCheckpointIndex = 0;
 	private GameObject currCheckpoint;
 	private Animator anim;
-	private float originaly;
+	private GameObject oldCheckpoint;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update and is used for initialization
     void Start()
     {
 		anim = GetComponentInChildren<Animator>();
+		// selecting and looking at the first checkpoint
 		currCheckpoint = checkpoints[currCheckpointIndex];
 		transform.LookAt(currCheckpoint.transform);
+		// This statement sets the hopping parameter to true triggering the hops
+		anim.SetBool("hopping", true);
     }
 
     // Update is called once per frame
     void Update()
     {
 		SetCheckpoint();
+
+		// angles rabbit towards checkpoint
 		Vector3 targetPosition = new Vector3(currCheckpoint.transform.position.x, 2.36f, currCheckpoint.transform.position.z);
 		transform.LookAt(targetPosition, Vector3.up);
     }
 
+	// updates target checkpoint when we've reached the current one
 	private void SetCheckpoint()
 	{
 		float dist = Vector3.Distance(currCheckpoint.transform.position, transform.position);
@@ -34,25 +40,26 @@ public class RabbitBehaviour : MonoBehaviour
 		{
 			currCheckpointIndex++;
 			currCheckpoint = checkpoints[currCheckpointIndex];
-			anim.SetBool("hopping", true);
-			//transform.LookAt(currCheckpoint.transform);
 		}
 	}
 
 	private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "tree_basic")
+        if (other.gameObject.name == "tree" || other.gameObject.name == "bush" || other.gameObject.name == "rock")
 		{
 			transform.LookAt(other.gameObject.transform.position);
-			Debug.Log(other.gameObject.transform.position);
 			anim.SetBool("hopping", false);
 			int explore = Random.Range(1, 3);
 			anim.SetInteger("explore", explore);
+			oldCheckpoint = currCheckpoint;
+			currCheckpoint = other.gameObject;
+			Debug.Log(oldCheckpoint.gameObject.name);
 		}
     }
 
 	private void RenableHopping()
 	{
+		currCheckpoint = oldCheckpoint;
 		anim.SetInteger("explore", 0);
 		anim.SetBool("hopping", true);
 	}
